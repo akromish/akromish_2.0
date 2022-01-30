40c1b390465d65d2b638d5d54ea7c170919dcd25
akram <akram.weheba.11@gmail.com>
2022-01-30 12:02:54 -0700

    added social links to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Avatar, Stack } from '@mui/material';
+import { Avatar, Link, Stack } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Home() {
   return (
@@ -10,6 +12,15 @@ export default function Home() {
         src="/akram-main.jpeg"
         sx={{ width: 250, height: 250 }}
       />
+      <Stack direction="row" spacing={2}>
+        {/* TODO: make links open new tabs */}
+        <Link href="https://github.com/akromish">
+          <GitHubIcon />
+        </Link>
+        <Link href="https://github.com/akromish">
+          <LinkedInIcon />
+        </Link>
+      </Stack>
     </Stack>
   );
 }
