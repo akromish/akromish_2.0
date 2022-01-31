c0dadd0c6d5c4bb0b0acb0bf6278f6fa30baa467
akram <akram.weheba.11@gmail.com>
2022-01-30 19:12:32 -0700

    typography + color

--- src/pages/RamblingsAndSuch.jsx
+++ src/pages/RamblingsAndSuch.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
 export default function RamblingsAndSuch() {
   return (
@@ -7,15 +7,14 @@ export default function RamblingsAndSuch() {
       <Box fontSize={{ xs: '12vw', lg: '4vw' }}>Ramblings and Such</Box>
       <Stack spacing={2}>
         <Stack spacing={1}>
-          <Box fontSize={{ xs: '8vw', lg: '2vw' }}>
+          <Typography fontSize={{ xs: '8vw', lg: '2vw' }} color="primary">
             Ramble 1.0
-          </Box>
-          <Box fontSize={{ xs: '5vw', lg: '1.25vw' }}>
+          </Typography>
+          <Typography fontSize={{ xs: '5vw', lg: '1.25vw' }}>
             Once upon a time, there was a member of the bourgeoisie.
             It was a young man. A mysterious one to be sure...
             <br />
             <br />
-
             His eyes glistened as he focused his gaze upon the noble Bob. &quot;Bob&quot;
             he exclaimed! What had Bob done this time. One would hope it
             was not a repeat of the horseshoe incident...lest we want another war
@@ -29,7 +28,7 @@ export default function RamblingsAndSuch() {
             invited. It was the real deal, as is commonly said. Seemingly,
             any time there is joy to be had, Bob gravitates towards preventing
             it. This festival was no different. Bam! The...
-          </Box>
+          </Typography>
         </Stack>
       </Stack>
       <div />
